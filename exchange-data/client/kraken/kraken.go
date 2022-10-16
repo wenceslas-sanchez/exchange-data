dca3faa6f221dec8ed3a36598e07d142d84b522f
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-10-16 21:45:32 +0200

    🎨 change New method

--- exchange-data/client/kraken/kraken.go
+++ exchange-data/client/kraken/kraken.go
@@ -7,25 +7,28 @@ import (
 const KRAKEN_URL = "https://api.kraken.com/0"
 
 type KrakenResponse struct {
-	Error error
-	data  *utils.Schema
+	Error  error
+	Result any
 }
 
 // A KrakenClient store all the needed information to request the Kraken's API.
 type KrakenClient struct {
 	// Go's base HTTP client.
 	Client *http.Client
-	// Kraken's API URL
-	URL string `default:"https://api.kraken.com/0"`
 	*http.Header
+	// Kraken's API URL
+	URL string
 }
 
 // New allow to construct a KrakenClient.
 //
 // client argument is not mandatory, if not provided, then the Go's HTTP client is used.
-func New(url string, header *http.Header, client *http.Client) *KrakenClient {
+func New(header *http.Header, client *http.Client, url string) *KrakenClient {
 	if client == nil {
 		client = &http.Client{}
 	}
-	return &KrakenClient{client, url, header}
+	if url == "" {
+		url = KRAKEN_URL
+	}
+	return &KrakenClient{client, header, url}
 }
