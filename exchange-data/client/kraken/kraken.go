21c32c07dc7cbca4f81008986a6ec0058537f8bc
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-10-16 18:37:55 +0200

    ⚗ testing kraken client

---
+++ exchange-data/client/kraken/kraken.go
@@ -0,0 +1,38 @@
+package kraken
+
+import (
+	"net/http"
+	"time"
+
+	"exchanges-data/exchange-data/utils"
+)
+
+const KRAKEN_URL = "https://api.kraken.com/0"
+
+type KrakenResponse struct {
+	Error error
+	data  *utils.Schema
+}
+
+// A KrakenClient store all the needed information to request the Kraken's API.
+type KrakenClient struct {
+	// Go's base HTTP client.
+	Client *http.Client
+	// Kraken's API URL
+	URL string
+	*http.Header
+}
+
+// New allow to construct a KrakenClient.
+//
+// client argument is not mandatory, if not provided, then the Go's HTTP client is used.
+func New(url string, header *http.Header, client *http.Client) *KrakenClient {
+	if client == nil {
+		client = &http.Client{}
+	}
+	return &KrakenClient{client, url, header}
+}
+
+func (k *KrakenClient) GetTrades(pair string, start, end time.Time) {
+
+}
